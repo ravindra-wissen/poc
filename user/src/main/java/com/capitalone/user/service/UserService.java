892419853cd621f02cced77708c618dbac3a5b36
Ravi Nalawade <ravindra.nalawade@wissen.com>
2021-10-03 15:37:16 +0530

    Test commit

--- user/src/main/java/com/capitalone/user/service/UserService.java
+++ user/src/main/java/com/capitalone/user/service/UserService.java
@@ -12,7 +12,7 @@ public class UserService {
 
 	public List<User> getAllUsers() {
 		List<User> users = new ArrayList<>();
-		User user1 = new User(1l, "Ravi");
+		User user1 = new User(1l, "Rani");
 		User user2 = new User(2l, "Raj");
 		User user3 = new User(3l, "Pooja");
 
