792df49c4e08be44efc3d8ff9c37ed66379f86cd
Ravi Nalawade <ravindra.nalawade@wissen.com>
2021-10-08 19:58:26 +0530

    added docker for naming server. enabled discovery client for exchange service

--- currency-exchange-service/src/main/java/com/capitalone/CurrencyExchangeServiceApplication.java
+++ currency-exchange-service/src/main/java/com/capitalone/CurrencyExchangeServiceApplication.java
@@ -3,8 +3,9 @@ package com.capitalone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-//Ravi @EnableDiscoveryClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CurrencyExchangeServiceApplication {
 
