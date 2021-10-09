2452523ac439610aa2d6139bfad580568f3b0ed0
Ravi Nalawade <ravindra.nalawade@wissen.com>
2021-10-09 17:41:12 +0530

    Added hard coded exchange server host

--- currency-conversion-service/src/main/java/com/capitalone/proxy/ExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/capitalone/proxy/ExchangeProxy.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.capitalone.model.Exchange;
 
-@FeignClient(name = "exchange/exchange")
+@FeignClient(name = "exchange", url = "http://dev-s-ecsal-1w4btirszwj1l-2099585939.us-east-2.elb.amazonaws.com")
 public interface ExchangeProxy {
 
 	@GetMapping("exchange/from/{from}/to/{to}")
