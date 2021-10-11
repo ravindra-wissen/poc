52b9b9c24c2f02d0af93884250d9d3a8b9781437
Ravi Nalawade <ravindra.nalawade@wissen.com>
2021-10-11 08:17:31 +0530

    changed lb

--- currency-conversion-service/src/main/java/com/capitalone/service/ConversionService.java
+++ currency-conversion-service/src/main/java/com/capitalone/service/ConversionService.java
@@ -24,7 +24,7 @@ public class ConversionService {
 		map.put("to", to);
 
 		RestTemplate t = new RestTemplate();
-		ResponseEntity<Exchange> forEntity = t.getForEntity("http://dev-s-ecsal-jor08gxbjy1c-1710585645.us-east-2.elb.amazonaws.com/exchange/from/{from}/to/{to}", 
+		ResponseEntity<Exchange> forEntity = t.getForEntity("http://dev-lb-22205762.us-east-2.elb.amazonaws.com//exchange/from/{from}/to/{to}", 
 				Exchange.class, map);
 		Exchange exchange = forEntity.getBody();
 		if(exchange == null) {
